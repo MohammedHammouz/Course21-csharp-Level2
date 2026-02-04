606d5ff292cfccc6dc1ecb7c3c8b789709717dd0
moha327 <m7amad9595@outlook.com>
2026-02-04 18:15:52 +0200

    Nullabl Data

---
+++ NullableData/NullableData/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullableData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nullable<int> nullableInt1 = null;
+            if (nullableInt1.HasValue)
+            {
+                Console.WriteLine($"nulableInt1 has value : {nullableInt1.Value}");
+            }
+            else{
+                Console.WriteLine("nulablleInt1 has no value");
+            }
+            int? nullableInt2 = 10;
+            if (nullableInt2.HasValue)
+            {
+                Console.WriteLine($"nulableInt1 has value : {nullableInt2.Value}");
+            }
+            else
+            {
+                Console.WriteLine("nulablleInt2 has no value");
+            }
+        }
+    }
+}
