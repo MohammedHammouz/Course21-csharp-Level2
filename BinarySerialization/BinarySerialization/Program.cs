cfd131efc7f0b4bfca13703119ca968b3c476096
moha327 <m7amad9595@outlook.com>
2026-02-12 21:34:07 +0200

    Serialization

---
+++ BinarySerialization/BinarySerialization/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+namespace BinarySerialization
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person { Name = "Mohammed Hammouz", Age = 31 };
+            BinaryFormatter formatter = new BinaryFormatter();
+            using(FileStream stream=new FileStream("Person.bin", FileMode.Create))
+            {
+                formatter.Serialize(stream, person);
+            }
+            using(FileStream stream =new FileStream("Person.bin", FileMode.Open))
+            {
+                Person DeserializePerson = (Person)formatter.Deserialize(stream);
+                Console.WriteLine($"Name:{DeserializePerson.Name},Age:{DeserializePerson.Age}");
+            }
+        }
+    }
+}
---
+++ JSONSerialization/JSONSerialization/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+namespace JSONSerialization
+{
+    [Serializable]
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person { Name = "Mohammed Hammouz", Age = 31 };
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Person));
+            using(MemoryStream stream=new MemoryStream())
+            {
+                serializer.WriteObject(stream, person);
+                string JSONString = Encoding.UTF8.GetString(stream.ToArray());
+                File.WriteAllText("Person.Json", JSONString);
+            }
+            using(FileStream stream=new FileStream("Person.Json", FileMode.Open))
+            {
+                Person DeserializablePerson = (Person)serializer.ReadObject(stream);
+                Console.WriteLine($"Name:{DeserializablePerson.Name},Age:{DeserializablePerson.Age}");
+            }
+        }
+    }
+}
