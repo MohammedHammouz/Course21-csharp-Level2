9a11f4d0a95c52d9f885fdd523b4f8522cbc4155
moha327 <m7amad9595@outlook.com>
2026-02-23 15:52:58 +0200

    TypeClassReflection

---
+++ TypeClassReflection/TypeClassReflection/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace TypeClassReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Type myType = typeof(string);
+            Console.WriteLine($"Name:{myType.Name}");
+            Console.WriteLine($"Is Class:{myType.IsClass}");
+            Console.WriteLine($"Assembly:{myType.Assembly}");
+            Console.WriteLine($"Full Name:{myType.FullName}");  
+        }
+    }
+}
