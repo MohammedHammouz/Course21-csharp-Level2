b952fcba44dcae8432df3036c67c11ec3d1bf67a
moha327 <m7amad9595@outlook.com>
2026-02-23 15:51:09 +0200

    ObsoleteAttributes

---
+++ ObsoleteAttributes/ObsoleteAttributes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObsoleteAttributes
+{
+    internal class Program
+    {
+        [Obsolete("This method1 is marked as obsetele method please use method2")]
+        public static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+        public static void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+        static void Main(string[] args)
+        {
+            Method1();
+            Method2();
+        }
+    }
+}
