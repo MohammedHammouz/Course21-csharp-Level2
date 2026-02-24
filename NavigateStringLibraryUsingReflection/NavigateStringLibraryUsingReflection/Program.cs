28552af720db1e32c50350f4931daea9db73d75b
moha327 <m7amad9595@outlook.com>
2026-02-24 12:36:23 +0200

    NavigateStringLibraryUsingReflection

---
+++ NavigateStringLibraryUsingReflection/NavigateStringLibraryUsingReflection/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+namespace NavigateStringLibraryUsingReflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly mscorlib=typeof(string).Assembly;
+            Type stringType = mscorlib.GetType("System.String");
+            if (stringType != null)
+            {
+                Console.WriteLine("Methods of System.String class:\n");
+                var stringMethods = stringType.GetMethods(BindingFlags.Instance | BindingFlags.Public).OrderBy(method => method.Name);
+                foreach(var method in stringMethods)
+                {
+                    Console.WriteLine($"{method.ReturnType} {method.Name}({GetParameterList(method.GetParameters())})");
+                }
+            }
+            
+        }
+        static string GetParameterList(ParameterInfo[] parameters)
+        {
+            return string.Join(",", parameters.Select(parameter => $"{parameter.ParameterType},{parameter.Name}"));
+        }
+        
+    }
+}
