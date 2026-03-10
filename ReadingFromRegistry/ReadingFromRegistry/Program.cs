aad97da960f45fdac188bed6cd7ba9ce0084f1a4
moha327 <m7amad9595@outlook.com>
2026-03-10 12:22:36 +0200

    ReadingFromRegistry

---
+++ ReadingFromRegistry/ReadingFromRegistry/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+namespace ReadingFromRegistry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string KeyPath = @"HKEY_CURRENT_USER\Software\MySoftWare";
+            string ValueName = "UserName";
+            string value = Registry.GetValue(KeyPath, ValueName, null) as string;
+            try
+            {
+                if (value != null)
+                {
+                    Console.WriteLine($"The value of {ValueName} is: {value}");
+                }
+                else
+                {
+                    Console.WriteLine($"Value {ValueName} not found in the Registry.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
