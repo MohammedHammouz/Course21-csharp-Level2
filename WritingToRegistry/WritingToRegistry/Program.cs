942f2201fbe9552e00549c4fa738b75eeddce964
moha327 <m7amad9595@outlook.com>
2026-03-10 12:23:25 +0200

    WritingToRegistry

---
+++ WritingToRegistry/WritingToRegistry/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Win32;
+namespace WritingToRegistry
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string KeyPath = @"HKEY_CURRENT_USER\Software\MySoftWare";
+            string ValueName = "UserName";
+            string ValueData = "User123";
+            try
+            {
+                Registry.SetValue(KeyPath, ValueName, ValueData, RegistryValueKind.String);
+                Console.WriteLine($"Value {ValueName} successfully written to the Registry.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
