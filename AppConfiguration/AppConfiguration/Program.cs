76cc2f0b8e77ddfbb7a0585a34e1d314d717228a
moha327 <m7amad9595@outlook.com>
2026-03-31 18:47:41 +0300

    AppConfiguration

---
+++ AppConfiguration/AppConfiguration/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+namespace AppConfiguration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConfigurationManager.AppSettings["Mohammed"]);
+            Console.WriteLine(ConfigurationManager.AppSettings["Skill1"]);
+            Console.WriteLine(ConfigurationManager.AppSettings["Skill2"]);
+            Console.WriteLine(ConfigurationManager.AppSettings["Skill3"]);
+        }
+    }
+}
