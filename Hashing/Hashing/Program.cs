af0199fe71335d4c3eca0927d488caf6bd731cb6
moha327 <m7amad9595@outlook.com>
2026-03-31 18:52:50 +0300

    Hashing

---
+++ Hashing/Hashing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+namespace Hashing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "Mohammed Al-Hammouz";
+            Console.WriteLine(data);
+            Console.WriteLine(ComputeHash(data));
+            Console.WriteLine(ComputeHash("Donkey"));
+            Console.ReadKey();
+        }
+        static string ComputeHash(string input)
+        {
+            using(SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                return BitConverter.ToString(hashBytes).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
