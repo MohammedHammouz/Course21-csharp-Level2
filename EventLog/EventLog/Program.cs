bcd0e1668f2d085fc9c2bfab09a1e8c32e896946
moha327 <m7amad9595@outlook.com>
2026-03-31 18:51:49 +0300

    EventLog

---
+++ EventLog/EventLog/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+namespace EventLog
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string SourceName = "MyApp";
+            if (!System.Diagnostics.EventLog.SourceExists(SourceName))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(SourceName, "Application");
+                Console.WriteLine("Event source created.");
+            }
+            System.Diagnostics.EventLog.WriteEntry(SourceName, "This is an information event.", EventLogEntryType.Information);
+            System.Diagnostics.EventLog.WriteEntry(SourceName, "This is an error event.", EventLogEntryType.Error);
+            System.Diagnostics.EventLog.WriteEntry(SourceName, "This is an warning event.", EventLogEntryType.Warning);
+        }
+    }
+}
